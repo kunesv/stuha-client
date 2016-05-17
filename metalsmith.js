738a4d25bb783bbe9714843b9086b845c64132c7
kunesv <kunesv@sprinters.cz>
2016-05-18 00:39:40 +0200

    Adding scss

--- metalsmith.js
+++ metalsmith.js
@@ -4,37 +4,42 @@ var markdown = require('metalsmith-markdown');
 var permalinks = require('metalsmith-permalinks');
 var dateFormatter = require('metalsmith-date-formatter');
 var layouts = require('metalsmith-layouts');
+var sass = require('metalsmith-sass');
 var serve = require('metalsmith-serve');
 var watch = require('metalsmith-watch');
 
+
 Metalsmith(__dirname)
     .use(collections({
-        "articles": {
-            "pattern": "articles/**/*.*",
-            "sortBy": "date",
-            "reverse": true
+        articles: {
+            pattern: 'articles/**/*.*',
+            sortBy: 'date',
+            reverse: true
         }
     }))
     .use(markdown())
     .use(permalinks(
         {
-            "pattern": ":title",
-            "relative": false
+            pattern: ':title',
+            relative: false
         }
     ))
     .use(dateFormatter({
-        "dates": [{
-            "key": "date",
-            "format": "MMM Do YYYY"
+        dates: [{
+            key: 'date',
+            format: 'MMM Do YYYY'
         }]
     }))
     .use(layouts({
-        "engine": "handlebars",
-        "directory": "src/layouts"
+        engine: 'handlebars',
+        directory: 'src/layouts'
+    }))
+    .use(sass({
+        outputDir: 'css/'
     }))
     .use(serve({
-        "host": "0.0.0.0",
-        "port": process.env.PORT || 5000
+        host: '0.0.0.0',
+        port: process.env.PORT || 5000
     }))
     .use(watch())
     .build(function (err) {

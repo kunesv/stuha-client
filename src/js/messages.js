1494ebd42012019b543b5ed20079d62bea5b4e9a
kunesv <vaclavkunes@sprinters.cz>
2016-08-18 01:05:16 +0200

    Preparation for tests

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,71 +1,8 @@
-[1, 2, 3, 4].map(messagePlaceholderAdd);
+initMessagePlaceholers();
 
-// TEST DATA preparation
-
-let currentUser = {userId: 2, userName: 'Houba'};
-
-let sampleMessages = [
-    'Tady scanuju celej byt.',
-    'Houba si balí na čundr: dva obří spacáky po prababičce a další pravou zálesáckou výbavu. Počítám, že asi tak po půl kilometru omdlí pod tou vahou únavou a povezete ho k tábořišti na kárce.',
-    'S Jendou případně jel já.',
-    'Já to včera u vás viděl, slušná nálož. Má aspoň ten bráchuv batoh? Kárku teda budeme muset vyrobit, asi z kulatin a klacků. :-)',
-    'Pche, to je všecko jen vzduch. To se nese samo.'
-];
-let sampleUsers = [
-    {
-        iconPath: '20_1',
-        userId: 20,
-        userName: 'Mokl'
-    },
-    {
-        iconPath: '3_4',
-        userId: 3,
-        userName: 'Sovička'
-    },
-    {
-        iconPath: '3_1',
-        userId: 3,
-        userName: 'Sovička'
-    },
-    {
-        iconPath: '2_2',
-        userId: 2,
-        userName: 'Houba'
-    }
-];
-let sampleImages = [
-    [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
-    [{url: 'img1.jpg'}],
-    [{url: 'img2.jpg'}],
-    [{url: 'img1.jpg'}, {url: 'img2.jpg'}],
-    [{url: 'img2.jpg'}, {url: 'img1.jpg'}],
-    [{url: 'img1.jpg'}, {url: 'img2.jpg'}, {url: 'img1.jpg'}]
-];
-
-
-let messages = [];
-let noOfMessages = Math.random() < .2 ? 0 : 10;
-for (let i = 0; i < noOfMessages; i++) {
-    let user = sampleUsers[Math.floor(Math.random() * 4)];
-    let date = new Date();
-    date.setMilliseconds(date.getMilliseconds() - i * 10000000);
-
-    let imagesId = Math.floor(Math.random() * 31);
-
-    messages.push({
-        id: i,
-        iconPath: user.iconPath,
-        userId: user.userId,
-        userName: user.userName,
-        formatted: imagesId != 30 ? sampleMessages[Math.floor(Math.random() * 5)] : '',
-        images: sampleImages[imagesId],
-        createdOn: date
-    });
+function initMessagePlaceholers() {
+    [1, 2, 3, 4].map(messagePlaceholderAdd);
 }
-messages.reverse();
-
-
-setTimeout(messagesLoadResult, 200);
 
 function messagesLoadResult() {
     if (Math.random() > .2) {
@@ -84,16 +21,15 @@ function messagesLoadResult() {
     }
 }
 
-
 function reload() {
     messageLoadFailedOverlayRemove();
 
-    setTimeout(messagesLoadResult, 500);
+    setTimeout(messagesLoadResult, Math.round(Math.random() * 1500));
 }
 
 function emptyDiscussionNoticeAdd() {
     let template =
-        `No messages found.`;
+        `<article>Tak tady se asi ještě diskuse moc nerozjela.</article>`;
 
     document.getElementsByClassName('messages')[0].insertAdjacentHTML('beforeend', template);
 }
@@ -101,7 +37,7 @@ function emptyDiscussionNoticeAdd() {
 function messageLoadFailedOverlayAdd() {
     let template =
         `<div class="overlay">
-            <span onclick="reload()">Load failed. <br/> Please reload.</span>
+            <span onclick="reload()">Zatím se nepodařilo nic nahrát. <br/> Zkuste to prosím ještě jednou.</span>
         </div>`;
 
     document.body.insertAdjacentHTML('beforeend', template);
@@ -198,3 +134,4 @@ function formatTime(date) {
 
     return ('0' + date.getHours()).slice(-2) + '.' + ('0' + date.getMinutes()).slice(-2);
 }
+

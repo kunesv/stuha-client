c8fb0e1f59568cf91cbae249b21240d771d5f51d
kunesv <vaclavkunes@sprinters.cz>
2016-08-22 03:08:19 +0200

    button animation test

--- src/js/animations.js
+++ src/js/animations.js
@@ -3,11 +3,15 @@ let buttons = document.getElementsByClassName('button');
 for (var i = 0; i < buttons.length; i++) {
     let button = buttons[i];
 
-    button.addEventListener('click', () => {
+    button.addEventListener('mousedown', () => {
         button.classList.add('clicked');
+    });
+
+    button.addEventListener('mouseup', () => {
         setTimeout(() => {
-            button.classList.remove('clicked')
+            button.classList.remove('clicked');
         }, 300)
+
     });
 }
 

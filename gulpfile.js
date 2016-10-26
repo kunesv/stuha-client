ed38e10653714f1e2fd5bafc4268fe77b18b79e9
kunesv <vaclavkunes@sprinters.cz>
2016-10-27 00:50:29 +0200

    Fetch API polyfill for that non-modern browser

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ var paths = {
     ],
     // JavaScript dependencies
     libs: [
-        './node_modules/babel-polyfill/dist/polyfill.js'
+        './node_modules/babel-polyfill/dist/polyfill.js',
+        './node_modules/whatwg-fetch/fetch.js'
     ],
     // These files are for your app's JavaScript
     app: [

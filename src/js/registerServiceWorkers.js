af8de58884d78d468a45fa75dd195810e97db3d4
kunesv <kunesv@sprinters.cz>
2017-10-31 00:01:33 +0100

    getting rid of 'createContextualFragment'

--- src/js/registerServiceWorkers.js
+++ src/js/registerServiceWorkers.js
@@ -7,22 +7,6 @@ window.onerror = function(msg, url, linenumber) {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
-
-    // .then(function (registration) {
-    //     return registration.pushManager.getSubscription().then(function (subscription) {
-    //         if (subscription) {
-    //             return subscription;
-    //         }
-    //
-    //         return registration.pushManager.subscribe({userVisibleOnly: true});
-    //     });
-    // }).then(function (subscription) {
-    //     let endpoint = subscription.endpoint;
-    //
-    //     console.log(endpoint)
-    // });
-
-
 } else {
     console.log('No Service Worker in navigator. Surfing anonymously?')
 }
